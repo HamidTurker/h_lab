b5a27d7981da02e9a31de2a71f9b38786c27641c
Hamid Turker <hamidturker@gmail.com>
2023-04-23 20:48:46 -0400

    Add files via upload

--- Tutorial_vandermeer.m
+++ Tutorial_vandermeer.m
@@ -9,6 +9,7 @@ clear all; clc
 %% Vandermeer codebase setup
 %addpath '/Users/hamid/Neuralynx/vdm/'
 %addpath(genpath('/Users/hamid/matlab/vandermeerlab/code-matlab/shared/'))
+addpath(genpath('/Users/hamid/matlab/matlab/hlab_matlab/vdm/'))
 
 %% Global directories and parameters
 path.tutorial = '/Users/hamid/Desktop/Research/+Github/vandermeerlab/';
@@ -44,12 +45,65 @@ cfg = []; cfg.Filter = [150 220]; % specify passband
 LFPfilt = h_FilterLFP(cfg,LFP);
 
 % extract the signal envelope by Hilbert transform
- 
-% detect intervals that pass a threshold
-cfg = []; cfg.method = 'zscore';
-cfg.threshold = 5; cfg.select =  '>'; % return intervals where threshold is exceeded
- 
-SWR = MakeIV(cfg,LFPfilt); % make intervals (corresponding to SWR events)
+
+%% Detect intervals that pass a threshold
+cfg = []; cfg.Method = 'zscore';
+cfg.Threshold = 5; cfg.Operation =  '>'; % return intervals where threshold is exceeded
+SWR = h_TSDtoIV(cfg,LFPfilt); % make intervals (corresponding to SWR events)
+
+
+
+
+
+%%%%%%%%%%%%
+
+
+
+%% Global directories and parameters
+path.base = '/Users/hamid/Desktop/Research/+Projects/Smith Lab/N-back_interference/+analyses/';
+path.ratfolder = '/Users/hamid/Desktop/Research/+Projects/Smith Lab/N-back_interference/CMTS_recording/R1813/R1813_2014-04-13/';
+path.cscdata = '/Users/hamid/Desktop/Research/+Projects/Smith Lab/N-back_interference/CMTS_recording/R1813/R1813_2014-04-13/CSC7.ncs';
+path.eventdata = '/Users/hamid/Desktop/Research/+Projects/Smith Lab/N-back_interference/CMTS_recording/R1813/R1813_2014-04-13/Events.nev';
+path.io = '/Users/hamid/matlab/fieldtrip-20230418/fileio/private';
+
+
+%% Load in CSC data
+% Since we're loading in a .ncs file, we need Fieldtrip's
+% read_neuralynx_ncs. We'll cd to the directory that has the input/output
+% scripts needed to read in the data. But, we're running the adapted
+% LoadCSC function from the VDM lab. This is done by setting up a so-called
+% configuration (cfg) structure and giving that to the function.
+cd(path.io)
+
+% Set up the cfg structure. Provide the path to the filename of interest
+% and ensure that it's in a cell array. Our original sampling rate is 32k
+% Hz. So, taking every 16th sample means we end up with 2k Hz.
+cfg = []; cfg.DecimateByFactor = 16;
+
+% Load CSC data from its location
+cfg.Flist = {path.cscdata};
+LFP = h_LoadCSC(cfg);
+
+% Load Event data from its location
+cfg.Elist = {path.eventdata};
+events = h_LoadEvents(cfg);
+
+%% Filter the data in the ripple band (150-220Hz)
+cd(path.ratfolder)
+cfg = []; cfg.Filter = [150 220]; % specify passband
+LFPfilt = h_FilterLFP(cfg,LFP);
+
+% extract the signal envelope by Hilbert transform
+
+%% Detect intervals that pass a threshold
+cfg = []; cfg.Method = 'zscore';
+cfg.Threshold = 5; cfg.Operation =  '>'; % return intervals where threshold is exceeded
+SWR = h_TSDtoIV(cfg,LFPfilt); % make intervals (corresponding to SWR events)
+
+
+
+
+
 
 
 
--- vdm/datatypes/iv/h_TSDtoIV.m
+++ vdm/datatypes/iv/h_TSDtoIV.m
@@ -39,8 +39,8 @@ iv_out = h_iv; % initialize new iv struct
 %% Check if conditions are in place
 nData = size(tsd_in.data,1);
 if nData > 1
-    if ~isempty(cfg.target)
-        temp_data = getd(tsd_in,cfg.target);
+    if ~isempty(cfg.Target)
+        temp_data = getd(tsd_in,cfg.Target);
     else
        error('Multiple data dimensions exist but no label is specified.'); 
     end
@@ -48,40 +48,40 @@ else
     temp_data = tsd_in.data;
 end
 
-% apply transform to data if requested
-thr_out = cfg.threshold;
-switch cfg.method
+%% Apply transform to data if requested
+thr_out = cfg.Threshold;
+switch cfg.Method
     case 'zscore'
         [temp_data,mu,sigma] = zscore(temp_data);
-        switch cfg.operation
+        switch cfg.Operation
             case '>' 
-                thr_out = mu + cfg.threshold.*sigma; % store threshold for output argument
+                thr_out = mu + cfg.Threshold.*sigma; % store threshold for output argument
             case '<'
-                thr_out = mu - cfg.threshold.*sigma;
+                thr_out = mu - cfg.Threshold.*sigma;
         end
     case 'percentile'
         temp_data_sorted = sort(temp_data,'ascend');
-        thr_out_idx = round(cfg.threshold.*length(temp_data));
+        thr_out_idx = round(cfg.Threshold.*length(temp_data));
         thr_out = temp_data_sorted(thr_out_idx);
         
         temp_data = tiedrank(temp_data)./length(temp_data); % assign percentile to each data point       
 end
 
-% detect crossings
-switch cfg.operation
+%% Detect crossings
+switch cfg.Operation
     case '>'
-        detec = temp_data > cfg.threshold;
+        detec = temp_data > cfg.Threshold;
     case '>='
-        detec = temp_data >= cfg.threshold;
+        detec = temp_data >= cfg.Threshold;
     case '<'
-        detec = temp_data < cfg.threshold;
+        detec = temp_data < cfg.Threshold;
     case '<='
-        detec = temp_data <= cfg.threshold;
+        detec = temp_data <= cfg.Threshold;
     case 'range'
-        detec = temp_data > cfg.threshold(1) & temp_data < cfg.threshold(2);
+        detec = temp_data > cfg.Threshold(1) & temp_data < cfg.Threshold(2);
 end
 
-% pad the detection so we can deal with cases where first or last samples are detects
+% Pad the detection so we can deal with cases where first or last samples are detects
 detec = cat(2,0,detec,0);
 
 dfs = diff(detec);
@@ -94,13 +94,13 @@ down_t = tsd_in.tvec(down_idx);
 % remove doubles
 d = up_t(2:end)-down_t(1:end-1);
 
-merge_idx = find(d < cfg.merge_thr);
+merge_idx = find(d < cfg.Merge_thr);
 down_t(merge_idx) = [];
 up_t(merge_idx+1) = [];
 
 % remove too short
 iv_len = down_t-up_t;
-keep_idx = iv_len > cfg.minlen;
+keep_idx = iv_len > cfg.Minlen;
 
 iv_out.tstart = up_t(keep_idx);
 iv_out.tend = down_t(keep_idx);
@@ -114,10 +114,10 @@ if ~iscolumn(iv_out.tend)
     iv_out.tend = iv_out.tend';
 end
 
-if cfg.verbose
+if cfg.Verbose
     disp([mfun,': ',num2str(length(iv_out.tstart)),' intervals found.'])
 end
 
-% housekeeping
+%% Housekeeping
 iv_out.cfg.history.mfun = cat(1,iv_out.cfg.history.mfun,mfun);
 iv_out.cfg.history.cfg = cat(1,iv_out.cfg.history.cfg,{cfg});
